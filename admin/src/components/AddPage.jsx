8cfd0cb8d93ec94c5a19289f29f45550fec9304c
e <appraisalsassets@gmail.com>
2026-07-02 12:37:53 +0500

    update admin url path

--- admin/src/components/AddPage.jsx
+++ admin/src/components/AddPage.jsx
@@ -18,7 +18,7 @@ const AddPage = () => {
     const [brandName, setBrandName] = useState("");
 
     const inputRef = useRef(null);
-    const API_BASE = "http://localhost:4000";
+const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:4000";
     const BRANDS = [
         "Rolex",
         "Omega",
